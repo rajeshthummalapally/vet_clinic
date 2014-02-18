78f2f71faf92a1a4797f8e69441db11b7cc38309
Rajesh Thummalapally <rajesh.thummalapally@gmail.com>
2014-02-18 08:07:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VetClinic::Application.config.secret_key_base = '5d8c543370fea61383fe7c7be86e3357592768ae2b1244f6b58b9ffc0a0564f7804902e5bcaed0923b263b4dfc5737913fce8f960ebeb8205d2da52149dd70ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VetClinic::Application.config.session_store :cookie_store, key: '_vet_clinic_session'
